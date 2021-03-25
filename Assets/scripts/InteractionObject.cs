4b90c232cd754a221a93debc3c17a671ffdc5049
williamfr7 <70350291+William-Fraser@users.noreply.github.com>
2021-03-25 08:37:07 -0300

    Added Interactablility

---
+++ Assets/scripts/InteractionObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionObject : MonoBehaviour
+{
+    [Header("Information Object")]
+    [Tooltip("Object gives small amount of info about themselves")]
+    public bool info = false;
+    [Tooltip("Displayed Text")]
+    public string message;
+    [Tooltip("Amount of time until message dissappears")]
+    public float displayTime;
+    private Text infoText;
+
+    [Header("Collectable Object")]
+    [Tooltip("Object can be picked up")]
+    public bool pickup = false;
+
+    private Interactable _InteractableState;
+    private enum Interactable{ 
+        STATE_NULL,
+        STATE_INFO,
+        STATE_PICKUP
+    
+    };
+
+    void Start()
+    {
+        infoText = GameObject.Find("InfoText").GetComponent<Text>();
+    }
+    public void Info()
+    {
+        Debug.Log("this is a "+ this.gameObject.name);
+        StartCoroutine(ShowInfo(message, displayTime));
+    }
+    public void Pickup()
+    {
+        Debug.Log("picked up a " + this.gameObject.name);
+        this.gameObject.SetActive(false);
+    }
+    IEnumerator ShowInfo(string message, float delay)
+    {
+        infoText.text = message;
+        yield return new WaitForSeconds(delay);
+        infoText.text = null;
+    }
+}
---
+++ Assets/scripts/PlayerInteraction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public GameObject currentInterObj = null;
+    public InteractionObject currentInterObjScript = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && currentInterObj)
+        {
+            if (currentInterObjScript.pickup)
+            {
+                currentInterObjScript.Pickup();
+            }
+            if (currentInterObjScript.info)
+            {
+                currentInterObjScript.Info();
+            }
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("InterObject"))
+        {
+            currentInterObj = collision.gameObject;
+            currentInterObjScript = currentInterObj.GetComponent<InteractionObject>();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("InterObject"))
+        {
+            currentInterObj = null;
+        }
+    }
+}
